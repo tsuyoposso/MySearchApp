b942bc27b93f441524ed60f9a319be9bbf691000
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-09-25 17:13:26 +0900

    JSONで取得するurlをUrlでも取得できるようにする処理でタイポしていた

--- MySearchApp/MySearchApp/ItemSearchResultSet.swift
+++ MySearchApp/MySearchApp/ItemSearchResultSet.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// 検索結果全体を格納するクラス
 class ItemSearchResultSet: Codable {
     var resultSet: ResultSet
     
@@ -16,6 +17,7 @@ class ItemSearchResultSet: Codable {
     }
 }
 
+// 検索結果セット格納クラス
 class ResultSet: Codable {
     var firstObject: FirstObject
     private enum CodingKeys: String, CodingKey {
@@ -23,6 +25,7 @@ class ResultSet: Codable {
     }
 }
 
+// 検索結果の先頭を格納するクラス
 class FirstObject: Codable {
     var result: Result
     private enum CodingKeys: String, CodingKey {
@@ -30,31 +33,31 @@ class FirstObject: Codable {
     }
 }
 
-
+// 検索結果格納クラス
 class Result: Codable {
     var items: [ItemData] = [ItemData]()
     required init(from decoder: Decoder) throws {
-        
+        // デコードのためのコンテナを取得
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        
+        // コンテナ内のキーを取得。キーが文字列であるため、そう内の照準でソートも行う
         let keys = container.allKeys.sorted {
             Int($0.rawValue)! < Int($1.rawValue)!
         }
-        
+        // キーを使用して検索結果を一件ずつ取り出す
         for key in keys {
-            
+            // 検索結果一軒に対するデコード処理
             let item = try container.decode(ItemData.self, forKey: key)
-            
+            // デコード処理できたら検索結果の一覧に追加
             items.append(item)
         }
     }
     
-    
+    // エンコード処理
     func encode(to encoder: Encoder) throws {
         // レスポンスを解析するだけなので、実装不要
     }
     
-    
+    // Resultクラスが持つ値を取得するためのキー
     private enum CodingKeys: String, CodingKey {
         case hit0 = "0"
         case hit1 = "1"
@@ -80,40 +83,44 @@ class Result: Codable {
     }
 }
 
-
+// 商品情報格納クラス
 class ItemData: Codable {
     
-    
+    // 商品名
     var name: String = ""
-    
+    // 商品URL
     var url: String = ""
     
+    // 商品画像情報
     class ImageInfo: Codable {
-        
+        // Imageクラスが持つ値を取得するためのキー
         private enum CodingKeys: String, CodingKey {
             case medium = "Medium"
         }
         
+        // 商品画像URL
         var medium: String?
     }
-    
+    // 商品画像URL
     var imageInfo: ImageInfo = ImageInfo()
     
-    
+    // 価格情報
     class PriceInfo: Codable {
-        
+        // Priceクラスが持つ値を取得するためのキー
         private enum CodingKeys: String, CodingKey {
             case price = "_value"
         }
         
+        // 価格
         var price: String?
     }
     
+    // 価格
     var priceInfo: PriceInfo = PriceInfo()
     
     private enum CodingKeys: String, CodingKey {
         case name = "Name"
-        case url = "url"
+        case url = "Url"
         case imageInfo = "Image"
         case priceInfo = "Price"
     }
